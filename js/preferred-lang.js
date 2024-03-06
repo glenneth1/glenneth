9ad90324189187b21240f0147b8902a0affdd7d1
glenneth1 <glennt40@gmail.com>
2024-03-06 09:20:59 +0300

    test changes

---
+++ js/preferred-lang.js
@@ -0,0 +1,1 @@
+(()=>{var n={fallback:"https://glenneth1.github.io/",homes:{en:"https://glenneth1.github.io/"}};(()=>{let o=navigator.language||navigator.userLanguage;if(o in n.homes){window.location.href=n.homes[o];return}let a=o.split("-");for(let e in n.homes)if(e.indexOf(a[0])===0){window.location.href=n.homes[e];return}window.location.href=n.fallback})();})();
